d4038f7f83944360ecf2988f95a909bbdb4fd17a
notashwinii <ashwinii.subedi@gmail.com>
2024-11-05 22:49:38 +0545

    add polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar --reload main &
+
+echo "Polybar launched..."
